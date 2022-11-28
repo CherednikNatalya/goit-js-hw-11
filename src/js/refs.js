b524155e3fbd8e3013c37ec1811b109a4809c266
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-11-28 19:16:23 +0200

    23

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,5 @@
 
-export {refs};
-
-refs = {
+export const refs = {
   form: document.querySelector("#search-form"),
   gallery: document.querySelector(".gallery"),
   btnMore: document.querySelector(".load-more"),
